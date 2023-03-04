fcb72c1c174e1d503d1aeab08405d4ab414619fe
Aline Cegatto <aline.guttner@gmail.com>
2023-03-04 17:15:59 -0300

    commit on 'main'

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const nome = document.getElementById('nome')
+const nome = document.getElementById('name')
 const email = document.getElementById('email')
-const submit = document.getElementsByClassName('submit')[0]
+const submit = document.getElementsByClassName('form-contact')[0]
 
 submit.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('hi')
+    console.log(email.value)
+
+    const textBody = `
+    <b>Nome: </b> ${nome.value}
+    <br>
+    <b>Email: </b> ${email.value}
+    `
+
 
     Email.send({
-        SecureToken: "509c7ac1-f303-4f82-b439-139b720bc1e4",
-        To: "aline.guttner@gmail.com",
+        SecureToken: "609bc9f4-a55a-4656-a521-60c868c01656",
+        To: "jangadafest@gmail.com",
         From: "jangadafest@gmail.com",
         Subject: "This is the subject",
-        Body: "And this is the body"
+        Body: textBody
     }).then(
         message => alert(message)
     );
