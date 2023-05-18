04a1a50ffa1dbf67ba211d9c03f37c6e15a33758
Lelly Oliver <lellyoliver@outlook.com>
2023-05-18 19:11:19 -0300

    update

--- index.js
+++ index.js
@@ -1,95 +1,76 @@
-const name = document.getElementById('name')
-const art = document.getElementById('art')
-const social = document.getElementById('social')
-const place = document.getElementById('place')
-const drive = document.getElementById('drive')
-const email = document.getElementById('email')
-const submit = document.getElementsByClassName('form-contact')[0]
+function getSubmitEmail() {
+    const submit = document.getElementsByClassName('form-contact')[0];
 
-submit.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(email.value)
+    submit.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const textBody = `
+        const formData = new FormData();
+        formData.append('Nome', name.value);
+        formData.append('Arte', art.value);
+        formData.append('Local', place.value);
+        formData.append('Social Media', socialmedia.value);
+        formData.append('Whatsapp', whatsapp.value);
+        formData.append('Google Drive', gdrive.value);
+        formData.append('Email', mail.value);
+
+        const forJangada = `
     <b>Nome: </b> ${name.value}
     <br>
-    <b>Fale resumidamente sobre a sua arte: </b> ${art.value}
+    <b>Fale resumidamente sobre sua arte: </b> ${art.value}
     <br>
     <b>Local onde atua: </b> ${place.value}
     <br>
-    <b>Rede social (preferência Instagram): </b> ${social.value}
+    <b>Whatsapp: </b> ${whatsapp.value}
     <br>
-    <b>Google Drive: </b> ${drive.value}
+    <b>Social Media: </b> ${socialmedia.value}
     <br>
-    <b>Email: </b> ${email.value}
-    `
-
-    const regulamento = `
-    
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-        <td align="center">
-        <a href="https://freeimage.host/"><img src="https://iili.io/HOYmW9j.png" alt="HOYmW9j.png" border="0" width="250px"></a>
-        </td>
-    </tr>
-</table>
+    <b>Google Drive: </b> ${gdrive.value}
     <br>
-    <br>
-    <br>
-    <b>REGULAMENTO GERAL DO FESTIVAL JANGADA 2023</b>
-    <br>
-    <br>
-    <ul>
-    <li>Somente artistas autorais poderão se inscrever; </li>
-    <li>Inscritos menores de idade deverão apresentar autorização
-    assinada pelo responsável e estar acompanhados no dia do
-    Festival; </li>
-    <li>A venda dos artesanatos e das obras de artes como pinturas e
-    fotografia será permitido - de imediato - sem custo aos artistas;</li>
-    <li>O tempo de apresentação das bandas e outras atrações será
-    o mesmo para todos, e deverá ser respeitado, caso contrário
-    a participação nos próximos festivais poderá ser invalidada. O
-    tempo de apresentação máxima será de 1 hora e 25 minutos; </li>
-    <li>O evento terá seu limite até às 22h, respeitando o horário
-    permitido por lei.</li>
-    </ul>
-    `
+    <b>Email: </b> ${mail.value}
+    `;
+        const forInscrito = `regulamento`;
 
+        Email.send({
+            SecureToken: "509c7ac1-f303-4f82-b439-139b720bc1e4",
+            To: "jangadafestival@gmail.com",
+            From: "jangadafestival@gmail.com",
+            Subject: "Inscrição Jangada",
+            Body: forJangada
+        }).then(
+            message => {
+                alert(message);
 
-    Email.send({
-        SecureToken: "509c7ac1-f303-4f82-b439-139b720bc1e4",
-        To: "jangadafestival@gmail.com",
-        From: "jangadafestival@gmail.com",
-        Subject: "Inscrição Jangada",
-        Body: textBody
-    }).then(
-        message => alert(message)
-    );
+                // Enviar regulamento para o e-mail do usuário
+                Email.send({
+                    SecureToken: "509c7ac1-f303-4f82-b439-139b720bc1e4",
+                    To: mail.value,
+                    From: "jangadafestival@gmail.com",
+                    Subject: "Regulamento Jangada Festival",
+                    Body: forInscrito
+                }).then(
+                    message => alert(message)
+                );
+            }
+        );
+    });
+}
+getSubmitEmail();
 
-    Email.send({
-        SecureToken: "509c7ac1-f303-4f82-b439-139b720bc1e4",
-        To: email.value,
-        From: "jangadafestival@gmail.com",
-        Subject: "REGULAMENTO JANGADA FESTIVAL",
-        Body: regulamento
-    }).then(
-        message => alert(message)
-    );
-})
+function carousel() {
+    let items = document.querySelectorAll('.carousel .carousel-item')
 
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-items.forEach((el) => {
-    const minPerSlide = 3
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-
+    items.forEach((el) => {
+        const minPerSlide = 3
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+}
+carousel();
