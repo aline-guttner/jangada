516ee5ab817effbcf9a2b64499dfbd29a9feeb8b
Aline Guttner Cegatto <aline.guttner@gmail.com>
2023-08-04 19:51:00 -0300

    ajustando form

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function carousel() {
+    let items = document.querySelectorAll('.carousel .carousel-item')
+
+    items.forEach((el) => {
+        const minPerSlide = 3
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+}
+carousel();
 
     const submit = document.getElementsByClassName('form-contact')[0];
 
@@ -132,24 +150,7 @@
     });
 
 
-function carousel() {
-    let items = document.querySelectorAll('.carousel .carousel-item')
 
-    items.forEach((el) => {
-        const minPerSlide = 3
-        let next = el.nextElementSibling
-        for (var i = 1; i < minPerSlide; i++) {
-            if (!next) {
-                // wrap carousel by using first child
-                next = items[0]
-            }
-            let cloneChild = next.cloneNode(true)
-            el.appendChild(cloneChild.children[0])
-            next = next.nextElementSibling
-        }
-    })
-}
-carousel();
 
 
 const btn = document.getElementsByClassName('btnNewsletter')[0]
@@ -266,4 +267,5 @@ function generateCaptcha() {
     }
   })
 
+  
  
